a874bf1aad7b368efbe8dec6dba8205b7f8b0da9
Torey Hickman <torey@devbootcamp.com>
2017-05-22 10:39:13 -0400

    Stops referring to a student as "it" in the tests

--- part-1/spec/student-spec.js
+++ part-1/spec/student-spec.js
@@ -17,33 +17,33 @@ describe("Student", function(){
   });
 
   describe("the student's average score", function() {
-    it("is the average of all its scores rounded down to the nearest whole number", function() {
+    it("is the average of all the student's scores rounded down to the nearest whole number", function() {
       expect(student.averageScore()).toEqual(80);
     });
   });
 
   describe("the student's letter grade", function() {
-    it("is an A if its average score is 90 or greater", function() {
+    it("is an A if the student's average score is 90 or greater", function() {
       spyOn(student, "averageScore").and.returnValue(90);
       expect(student.letterGrade()).toEqual("A");
     });
 
-    it("is a B if its average score is 80 or greater but less than 90", function() {
+    it("is a B if the student's average score is 80 or greater but less than 90", function() {
       spyOn(student, "averageScore").and.returnValue(80);
       expect(student.letterGrade()).toEqual("B");
     });
 
-    it("is a C if its average score is 70 or greater but less than 80", function() {
+    it("is a C if the student's average score is 70 or greater but less than 80", function() {
       spyOn(student, "averageScore").and.returnValue(70);
       expect(student.letterGrade()).toEqual("C");
     });
 
-    it("is a D if its average score is 60 or greater but less than 70", function() {
+    it("is a D if the student's average score is 60 or greater but less than 70", function() {
       spyOn(student, "averageScore").and.returnValue(60);
       expect(student.letterGrade()).toEqual("D");
     });
 
-    it("is an F if its average score is less than 60", function() {
+    it("is an F if the student's average score is less than 60", function() {
       spyOn(student, "averageScore").and.returnValue(59);
       expect(student.letterGrade()).toEqual("F");
     });
