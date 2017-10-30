fae4fef31b10f1121e4e1a1045600289e879fb29
Taylor Svendsen <taysvendsen@gmail.com>
2017-10-30 11:04:19 -0700

    adds user and post migrations with seed file

---
+++ part-3/db/migrate/20171030104903_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.belongs_to :users, index: true
+      t.string :title, null: false
+      t.string :description, null: false
+      t.string :condition, null: false
+      t.decimal :price, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ part-3/db/seeds.rb
@@ -0,0 +1,15 @@
+require 'faker'
+
+users = []
+
+
+50.times do
+  users << User.create!(:first_name => Faker::Name.first_name, :last_name  => Faker::Name.last_name, :email => Faker::Internet.email, :password => 'password')
+end
+
+
+users.each do |user|
+  15.times do
+    user.posts << Post.create!(title: Faker::Lorem.word, description: Faker::Lorem.paragraph(5), condition: Faker::Lorem.sentence, price: Faker::Number.number(2))
+  end
+end
