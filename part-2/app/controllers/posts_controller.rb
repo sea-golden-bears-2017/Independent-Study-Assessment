a7c289735dec1ceffaf83face2eb85761925ba46
Taylor B. Svendsen <taysvendsen@gmail.com>
2017-10-31 16:34:15 -0700

    gets like to increase on page

--- part-2/app/controllers/posts_controller.rb
+++ part-2/app/controllers/posts_controller.rb
@@ -3,11 +3,7 @@ get "/posts" do
   erb :'posts/index'
 end
 
-# AJAX
 post "/posts" do
-  p " >.< " * 100
-  p params
-  p " >.< " * 100
   @post = Post.new(params[:post])
   if @post.save
     if request.xhr?
@@ -31,12 +27,12 @@ get "/posts/:id" do
   erb :'posts/show'
 end
 
-# AJAX
 put "/posts/:id/like" do
   @post = Post.find(params[:id])
   @post.increment!(:likes_count)
   if request.xhr?
-    erb :"posts/_post_like_form"
+    # p ")==(" * 100
+    erb :"posts/_post", locals: {post: @post}, layout: false
   else
     redirect "/posts/#{@post.id}"
   end
--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
   $("#new-post-form").on("submit", (e) => {
     e.preventDefault();
-    console.log("prevented default")
     const data = $("#new-post-form").serialize()
     $.ajax({
       type: 'POST',
       url: '/posts',
       data: data
     }).done((response)=>{
-      console.log(response)
       $("#post-list").prepend(`<li>${response}</li>`)
     });
   });
+
+  $("article").on("submit", (e) => {
+    e.preventDefault();
+    console.log("prevented default")
+    const url = $(e.target, "form").attr("action")
+    console.log(url)
+    $.ajax({
+      type: 'PUT',
+      url: url
+    }).done((response)=>{
+      console.log(response)
+      $(e.target).closest("article").html(response)
+    });
+  });
+
+
 });
