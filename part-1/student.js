65e31cc49e2c5aec7de4db6abafeb765270bcef0
Taylor B. Svendsen <taysvendsen@gmail.com>
2017-10-31 12:17:16 -0700

    creates student class

--- part-1/student.js
+++ part-1/student.js
@@ -2,3 +2,33 @@
 /*   It is required to use a for loop                   */
 /*   when iterating over a student's array of scores.   */
 /********************************************************/
+
+class Student {
+  constructor({firstName, scores}) {
+    this.firstName = firstName;
+    this.scores = scores;
+  }
+
+  averageScore() {
+    let scoreAverage = this.scores.reduce((a, b) => a + b)/this.scores.length;
+    return Math.trunc(scoreAverage)
+  }
+
+  letterGrade() {
+    if (this.averageScore() >= 90) {
+      return "A"
+    }
+    else if (this.averageScore() <= 90 && this.averageScore() >= 80) {
+      return "B"
+    }
+    else if (this.averageScore() <= 80 && this.averageScore() >= 70) {
+      return "C"
+    }
+    else if (this.averageScore() <= 70 && this.averageScore() >= 60) {
+      return "D"
+    } else {
+      return "F"
+    }
+  }
+
+}
