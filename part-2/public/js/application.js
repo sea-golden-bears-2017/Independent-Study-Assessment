fb9066d14b02c5aca96a028d254d011fbf3dc2ec
Taylor B. Svendsen <taysvendsen@gmail.com>
2017-10-31 16:48:48 -0700

    gets form field to reset on submission

--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
   $("#new-post-form").on("submit", (e) => {
     e.preventDefault();
-    const data = $("#new-post-form").serialize()
+    const data = $("#new-post-form").serialize();
     $.ajax({
       type: 'POST',
       url: '/posts',
       data: data
     }).done((response)=>{
-      $("#post-list").prepend(`<li>${response}</li>`)
+      $("#post-list").prepend(`<li>${response}</li>`);
+      $(e.target).closest("form")[0].reset();
     });
   });
 
   $("#post-list").on("submit", (e) => {
     e.preventDefault();
-    console.log("prevented default")
-    const url = $(e.target, "form").attr("action")
-    console.log(url)
+    console.log("prevented default");
+    const url = $(e.target, "form").attr("action");
+    console.log(url);
     $.ajax({
       type: 'PUT',
       url: url
     }).done((response)=>{
-      console.log(response)
-      $(e.target).closest("article").html(response)
+      console.log(response);
+      $(e.target).closest("article").html(response);
     });
   });
 
