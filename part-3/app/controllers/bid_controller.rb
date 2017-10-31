cd42efdaf78fe1b8dcef283c80e59a9aa28c91d5
Taylor Svendsen <taysvendsen@gmail.com>
2017-10-30 18:34:03 -0700

    lays out logic for bid feature, ran out of time

--- part-3/app/controllers/bid_controller.rb
+++ part-3/app/controllers/bid_controller.rb
@@ -1,3 +1,13 @@
-# post "/bids" do
-#   @bid = Bid.new(params[:bid])
-# end
+post "/bids" do
+  if #users hasn't bid on auction yet
+    @bid = Bid.new(params[:bid])
+    #associate with a user and auction
+    if @bid.save
+      #redirect
+    else
+      #redirect
+    end
+  else
+    #error
+  end
+end
