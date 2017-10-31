924e0312db763da2a98a7af461f9cabcdd0cf81c
Taylor B. Svendsen <taysvendsen@gmail.com>
2017-10-31 12:40:59 -0700

    adds logic for find class method

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -2,3 +2,21 @@
 /*   It is required to use Array.prototype methods          */
 /*   when iterating over a classroom's array of students.   */
 /************************************************************/
+
+class Classroom {
+  constructor(students) {
+    this.students = students
+  }
+
+  find(studentName) {
+    nameFunction(pupil) => {
+      return pupil.firstName === studentName
+    };
+    return this.students.find(nameFunction)
+  }
+
+  honorRollStudents() {
+    // return array of student objects
+  }
+
+}
