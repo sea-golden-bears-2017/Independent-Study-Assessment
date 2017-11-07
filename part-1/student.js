06b1c1525ee2c33a6d531c7c135044702c64faf0
Piper Denney <denney.piper@gmail.com>
2017-11-07 15:24:32 -0800

    Student class is defined. Student has firstName and scores. Student class finds average scores.

--- part-1/student.js
+++ part-1/student.js
@@ -2,3 +2,18 @@
 /*   It is required to use a for loop                   */
 /*   when iterating over a student's array of scores.   */
 /********************************************************/
+
+class Student {
+  constructor({firstName, scores}) {
+    this.firstName = firstName;
+    this.scores = scores;
+  }
+
+  averageScore () {
+    const total = this.scores.reduce((sum, value) => sum + value);
+    const avg = total / this.scores.length;
+    return Math.floor(avg);
+  }
+}
+
+window.Student = Student
