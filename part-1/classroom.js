9e929a94da86e88716a214f6a14de05bf09d1728
Piper Denney <denney.piper@gmail.com>
2017-11-07 15:53:31 -0800

    Classroom is defined and has students.

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -2,3 +2,9 @@
 /*   It is required to use Array.prototype methods          */
 /*   when iterating over a classroom's array of students.   */
 /************************************************************/
+class Classroom {
+  constructor(students) {
+    this.students = students;
+  }
+
+} //end of class
