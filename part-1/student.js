3b526ac57ebfa246ec5c8296b200c665543c3a06
Piper Denney <denney.piper@gmail.com>
2017-11-07 15:47:53 -0800

    Student has letter grades.

--- part-1/student.js
+++ part-1/student.js
@@ -14,6 +14,21 @@ class Student {
     const avg = total / this.scores.length;
     return Math.floor(avg);
   }
-}
+
+  letterGrade () {
+    if (this.averageScore() >= 90) {
+      return "A";
+    } else if (this.averageScore() >= 80) {
+      return "B";
+    } else if (this.averageScore() >= 70) {
+      return  "C";
+    } else if (this.averageScore() >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+
+} //end of class
 
 window.Student = Student
