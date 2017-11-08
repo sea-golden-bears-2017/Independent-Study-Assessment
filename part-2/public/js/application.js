b9f5e0f06dc2d093dff2a44033536eeb7c76db24
Piper Denney <denney.piper@gmail.com>
2017-11-07 17:26:14 -0800

    Adds comment to page without refreshing. Does not clear form yet.

--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -1,5 +1,15 @@
 $(document).ready(() => {
   $('#new-post-form').on('submit', (e) => {
     e.preventDefault();
-  })
+    const form = $(e.currentTarget);
+    const data = form.serialize();
+    const url = form.attr('action');
+    $.ajax({
+      url: url,
+      data: data,
+      method: 'POST'
+    }).done((response) => {
+      $('ul#post-list').prepend(`<li> ${response}</li>`);
+    });
+  });
 })
