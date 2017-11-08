b44305637cadd70b347faced3f62495e58466ccb
Piper Denney <denney.piper@gmail.com>
2017-11-08 15:41:24 -0800

    Add user model with bcrypt methods and model validations.

---
+++ part-3/app/models/user.rb
@@ -0,0 +1,23 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :hashed_password, presence: true
+
+
+
+
+
+
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+  
+end
