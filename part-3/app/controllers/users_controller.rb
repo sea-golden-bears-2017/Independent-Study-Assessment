84de6a7d06c819e807320f6a87c61caff2d48d5b
Piper Denney <denney.piper@gmail.com>
2017-11-08 16:37:13 -0800

    User can register account. (before loggin in).

---
+++ part-3/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  user.password = params[:password]
+  if user.save
+    session[:user_id] = user.id
+    @messages = ["Hello, #{user.username}!"]
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    p @errors
+    erb :'/users/new'
+  end
+end
+
+get 'users/show' do
+
+end
