2c176579f926de76b2e38387f482ef68c52da816
Piper Denney <denney.piper@gmail.com>
2017-11-08 19:37:36 -0800

    User can add an auction and be routed back to their profile where their list of auction items is displayed.

--- part-3/app/controllers/auctions_controller.rb
+++ part-3/app/controllers/auctions_controller.rb
@@ -4,7 +4,17 @@ get 'auctions' do
 end
 
 get '/auctions/new' do
-
     erb :"/auctions/new"
+end
 
+post '/auctions' do
+  @user = User.find(session[:user_id])
+  @auction = Auction.new(params[:auction])
+  @auction.user_id = session[:user_id]
+  if @auction.save
+    @user.auctions << @auction
+    redirect :"/users/#{@user.id}"
+  else
+    erb :"auctions/new"
+  end
 end
