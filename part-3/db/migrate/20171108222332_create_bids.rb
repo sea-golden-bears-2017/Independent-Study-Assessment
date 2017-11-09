acb77e4f3f651a1769f68430f0b1827e5c9132ee
Piper Denney <denney.piper@gmail.com>
2017-11-08 22:34:10 -0800

    Fix migration. Didn't provide argument for create table.

--- part-3/db/migrate/20171108222332_create_bids.rb
+++ part-3/db/migrate/20171108222332_create_bids.rb
@@ -1,6 +1,6 @@
 class CreateBids < ActiveRecord::Migration
   def change
-    create_table do |t|
+    create_table :bids do |t|
       t.belongs_to :user
       t.belongs_to :auction
       t.decimal :bid_amount, null: false #Read is good to use decimal for monetary value. Thoughts?
