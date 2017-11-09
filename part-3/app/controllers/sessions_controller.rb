b4454b8d952e0169763434c72c1a1a2c7e25c499
Piper Denney <denney.piper@gmail.com>
2017-11-08 16:36:18 -0800

    Sessions controller redirects to index if logged in. Delete clears session.

---
+++ part-3/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+get '/sessions/new' do
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  user = User.find_by_email(params[:email])
+  if user && user.password == params[:password]
+    session[:user_id] = user.id
+    # @messages = ["Hello, #{user.username}!"]
+    redirect '/'
+  else
+    @errors = ['Incorrect username or password.']
+    erb :'/sessions/new'
+  end
+end
+
+delete '/sessions/:id' do
+  session.clear
+  redirect '/'
+end
